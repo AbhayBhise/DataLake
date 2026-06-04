8604ad7bd06eb23bed2b53823b02a8661740e66a
Abhay <bhiseabhay890@gmail.com>
2026-06-04 13:48:29 +0530

    fix: suppress InteractionManager deprecation warning toast

--- App.tsx
+++ App.tsx
@@ -4,9 +4,14 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, LogBox } from 'react-native';
 import ErrorBoundary from './src/components/ErrorBoundary';
 import AppNavigator  from './src/navigation/AppNavigator';
+
+// Ignore specific warnings from third-party libraries (e.g. Navigation)
+LogBox.ignoreLogs([
+  'InteractionManager has been deprecated',
+]);
 
 const App: React.FC = () => {
   return (
